197db587b33e47832778fe85dac63ae2c310bcc3
Anqi Zhou <anqizhou89@gmail.com>
2014-10-16 14:37:48 +0800

    Landing page structure set up

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    if params[:admin] == "true"
+      @timeFutureShow = (Time.now + 2700).strftime("%I:%M %p")
+      render 'index'
+    else
+      render 'under_construction'
+    end
   end
+
+
 end
