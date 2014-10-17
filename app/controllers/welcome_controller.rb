7de60de0d5638980deff7c9a04fd8f9c3bba6b4b
Anqi Zhou <anqizhou89@gmail.com>
2014-10-17 15:36:59 +0800

    Set up database synced with page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,14 @@ class WelcomeController < ApplicationController
   def index
     if params[:admin] == "true"
       @timeFutureShow = (Time.now + 2700).strftime("%I:%M %p")
+
+      # Retrieve data
+      @foodItemAdventurous = FoodItem.where(cuisine: "Adventurous").sample
+      @foodItemAsian =  FoodItem.where(cuisine: "Asian").sample
+      @foodItemWestern = FoodItem.where(cuisine: "Western").sample
+
+
+
       render 'index'
     else
       render 'under_construction'
